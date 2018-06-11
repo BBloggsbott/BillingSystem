9952dddf21c1037a5dffa95788d1a69fcf3220af
BBloggsbott <vishal27.rs@gmail.com>
2018-06-11 17:06:54 +0530

    Add SearchProductFrame.java

---
+++ src/main/java/com/bbloggsbott/billingsystem/presentation/producthandling/SearchProductFrame.java
@@ -0,0 +1,92 @@
+package com.bbloggsbott.billingsystem.presentation.producthandling;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+import com.bbloggsbott.billingsystem.service.productservice.ProductLookup;
+
+public class SearchProductFrame extends JFrame implements ActionListener{
+    JPanel header, content;
+    JLabel title, idLabel, nameLabel;
+    JTextField id, name;
+    String[] columnNames = { "ID", "Name", "Buy Price", "Sell Price", "Type", "Stock" };;
+    JTable resultTable;
+    JScrollPane scrollPane;
+    Object[][] items;
+    DefaultTableModel resultModel;
+    public SearchProductFrame(){
+        
+        title = new JLabel("Search Product");
+        idLabel = new JLabel("ID");
+        nameLabel = new JLabel("Name");
+
+        id = new JTextField(8);
+        name = new JTextField(10);
+
+        header = new JPanel(new GridLayout(3, 1));
+        JPanel header1 = new JPanel(new FlowLayout());
+        JPanel header2 = new JPanel(new FlowLayout());
+        JPanel header3 = new JPanel(new FlowLayout());
+        header1.add(title);
+        header2.add(idLabel);
+        header2.add(id);
+        header3.add(nameLabel);
+        header3.add(name);
+
+        header.add(header1);
+        header.add(header2);
+        header.add(header3);
+
+        resultModel = new DefaultTableModel();
+        for (int i = 0; i < 6; i++) {
+            resultModel.addColumn(columnNames[i]);
+        }
+        resultTable = new JTable(resultModel);
+        scrollPane = new JScrollPane(resultTable);
+        resultTable.setFillsViewportHeight(true);
+
+        content = new JPanel(new GridLayout(1, 1));
+        content.add(scrollPane);
+
+        add(header, BorderLayout.NORTH);
+        add(content, BorderLayout.CENTER);
+
+        id.addActionListener(this);
+        name.addActionListener(this);
+
+        setSize(900, 400);
+        setVisible(true);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource() == id){
+            name.setText("");
+            items = ProductLookup.toTwoDArray(ProductLookup.lookupByID(Integer.parseInt(id.getText())));
+            resultModel.setRowCount(0);
+            for(int i=0;i<items.length;i++){
+                resultModel.addRow(items[i]);
+            }
+        }
+        else if(e.getSource() == name){
+            id.setText("");
+            items = ProductLookup.toTwoDArray(ProductLookup.lookupByName(name.getText()));
+            resultModel.setRowCount(0);
+            for (int i = 0; i < items.length; i++) {
+                resultModel.addRow(items[i]);
+            }
+        }
+    }
+}
